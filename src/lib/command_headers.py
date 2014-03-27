fe32bd48a29654fc260dcc00dde3b2f503608192
Gustavo Valdez <gvaldezsan@gmail.com>
2014-03-26 18:50:49 -0600

    Remove unused commands

--- src/lib/command_headers.py
+++ src/lib/command_headers.py
@@ -4,29 +4,8 @@ commands = {
 	'!test': {
 		'limit': 30,
 		'return': 'This is a test!'
-	},
-
-	'!randomemote': {
-		'limit': 180,
-		'argc': 0,
-		'return': 'command'
-	},
-
-	'!wow': {
-		'limit': 30,
-		'argc': 3,
-		'return': 'command'
 	}
 }
-
-
-
-
-
-
-
-
-
 
 for channel in config['channels']:
 	for command in commands:
